3fb21c3efa95af6f2ba8f5b5026c434db53c6050
desantamaria <santamaria.daniel316@gmail.com>
2023-11-28 15:23:23 -0800

    Add S3 Storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "pdfStorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
