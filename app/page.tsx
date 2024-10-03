f20cc59d3b895039cb25b67d07bd5e6b79c7514a
Daniel Santamaria <santamaria.daniel316@gmail.com>
2024-10-02 23:43:34 -0700

    Fix "SignIn" is not a valid Page export field.

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,25 @@ import { Button } from "@/components/ui/Button";
 import { redirect } from "next/navigation";
 
 export default function Home() {
+  async function SignIn() {
+    "use server";
+
+    // Skip sign-in screen if the user is already signed in
+    if ((await auth()) !== null) {
+      redirect("/loggedin");
+    }
+
+    await signIn(undefined, { redirectTo: "/loggedin" });
+  }
+
   return (
     <>
       <StickyHeader className="px-4 py-2">
         <div className="flex justify-between items-center">
           Convex + Next.js + Auth.js
-          <SignIn />
+          <form action={SignIn}>
+            <Button type="submit">Sign in</Button>
+          </form>
         </div>
       </StickyHeader>
       <main className="container max-w-2xl flex flex-col gap-8">
@@ -23,24 +36,5 @@ export default function Home() {
         <p>To interact with the app log in via the button up top.</p>
       </main>
     </>
-  );
-}
-
-export function SignIn() {
-  return (
-    <form
-      action={async () => {
-        "use server";
-
-        // Skip sign-in screen if the user is already signed in
-        if ((await auth()) !== null) {
-          redirect("/loggedin");
-        }
-
-        await signIn(undefined, { redirectTo: "/loggedin" });
-      }}
-    >
-      <Button type="submit">Sign in</Button>
-    </form>
   );
 }
