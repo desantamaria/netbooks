2eba1760d4411785145f6a75693678c20ecf5e43
Daniel Santamaria <santamaria.daniel316@gmail.com>
2024-10-04 20:37:58 -0700

    Implement Forms into Settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,49 +1,22 @@
 "use client";
-import React from "react";
-import { Code } from "@/components/typography/code";
-import { Link } from "@/components/typography/link";
-import { Button } from "@/components/ui/Button";
+import { Separator } from "@/components/ui/separator";
+import { ProfileForm } from "./profile-form";
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
-import { Skeleton } from "@/components/ui/skeleton";
 
-export default function LoggedInHome() {
+export default function SettingsProfilePage() {
   const viewerInfo = useQuery(api.myFunctions.getUserInfo);
-
-  if (viewerInfo === undefined) {
-    return (
-      <div className="mt-8">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px] mt-4" />
-      </div>
-    );
-  }
-
-  if (viewerInfo.length === 0) {
-    return <p className="mt-8">No user information found.</p>;
-  }
-
-  const user = viewerInfo[0];
-
+  console.log(viewerInfo);
   return (
-    <div className="flex flex-col  justify-center">
-      <p className="mt-8">Welcome {user.name}</p>
-      <p>Account JSON:</p>
-      <div className="mt-2 p-4 bg-gray-100 rounded w-[63vw]">
-        <pre>{JSON.stringify(user, null, 2)}</pre>
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Profile</h3>
+        <p className="text-sm text-muted-foreground">
+          This is how others will see you on the site.
+        </p>
       </div>
-      <div className="flex gap-2">
-        <p>Name: </p>
-        <p>{user.name}</p>
-      </div>
-      <div className="flex gap-2">
-        <p>Email: </p>
-        <p>{user.email}</p>
-      </div>
-      <div className="flex gap-2">
-        <p>ID: </p>
-        <p>{user._id}</p>
-      </div>
+      <Separator />
+      <ProfileForm />
     </div>
   );
 }
